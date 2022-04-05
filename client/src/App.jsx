454548053b7bc44f6855548f10383ad15c5cb797
DivineDemon <supame123@gmail.com>
2022-04-05 23:42:15 +0400

    An Upgrade to my Previous Chat App :sparkles:

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>MediPing - Slack for the Medical Professionals!</h1>
+    </div>
+  );
+};
+
+export default App;
