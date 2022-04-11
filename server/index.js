413f53a232b1dfa223f65b7bdb8c880d4b94f0ee
DivineDemon <supame123@gmail.com>
2022-04-12 02:38:20 +0400

    Added Twilio Messaging Service :sparkles: :rocket:

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const accountSID = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_KEY;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require("twilio")(accountSID, authToken);
 
 // Routes Import
 const authRoute = require("./routes/auth");
@@ -16,6 +20,28 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Base Route Working!" });
+});
+app.post("/", (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === "message.new") {
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: messagingServiceSid,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log("Message Sent!"))
+            .catch((error) => console.log(error));
+        }
+      });
+    return res.status(200).send("Message Sent!");
+  }
+  return res.status(200).send("Not a new message request!");
 });
 app.use("/auth", authRoute);
 
