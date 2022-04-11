72fb5198bce6acedd24a2507a95c0aae765ec61e
DivineDemon <supame123@gmail.com>
2022-04-12 03:07:43 +0400

    The App is Live! :sparkles: :rocket:

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -25,7 +25,7 @@ const Auth = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { username, password, phoneNumber, avatarURL } = form;
-    const URL = "http://localhost:5000/auth";
+    const URL = "https://medi-ping.herokuapp.com/auth";
     const {
       data: { token, userId, hashedPassword, fullName },
     } = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, {
